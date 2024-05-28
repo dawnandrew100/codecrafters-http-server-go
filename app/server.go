a32bd1c062753e8be28adb45a661e473338ea0da
Andrew Hennis <dawnandrew100@gmail.com>
2024-05-28 08:09:45 -0400

    Added message handling

--- app/server.go
+++ app/server.go
@@ -2,17 +2,16 @@ package main
 
 import (
 	"fmt"
-	// Uncomment this block to pass the first stage
 	 "net"
 	 "os"
+     "strings"
 )
 
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Println("Logs from your program will appear here!")
 
-	 // Uncomment this block to pass the first stage
-	
+    // Creates TCP server listening on port 4221	
 	 l, err := net.Listen("tcp", "0.0.0.0:4221")
 	 if err != nil {
 	 	fmt.Println("Failed to bind to port 4221")
@@ -25,11 +24,36 @@ func main() {
 	 	fmt.Println("Error accepting connection: ", err.Error())
 	 	os.Exit(1)
 	 }
-     defer conn.Close()
 
-     _, err = conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
-     if err != nil {
-         fmt.Println("Error writing response: ", err.Error())
-         os.Exit(1)
-     }
+     handleConnection(conn)
+}
+
+func handleConnection(conn net.Conn) {
+    //frees memory by closing connection at end of function
+    defer conn.Close()
+    
+    buf := make([]byte, 0, 1024)
+    conn.Read(buf)
+
+    bufString := strings.Split(string(buf), "\n")
+
+    request := strings.Split(bufString[0], " ")
+    // headers := strings.Split(bufString[1], " ")
+
+    // method := request[0]
+    path := request[1]
+    // version := request[2]
+    fmt.Printf("Path is %s:\n", path)
+
+    var response string = "HTTP/1.1 404 Not Found\r\n\r\n"
+    if path == "/" {
+        response == "HTTP/1.1 200 OK\r\n\r\n"
+    }
+
+    _, err = conn.Write([]byte(response))
+    if err != nil {
+        fmt.Println("Failed to write response")
+        os.Exit(1)
+    }
+
 }
