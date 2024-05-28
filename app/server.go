141be2996033ac3f059f71b6dc2ad3c253fb43fd
Andrew Hennis <dawnandrew100@gmail.com>
2024-05-28 10:41:30 -0400

    redo main func

--- app/server.go
+++ app/server.go
@@ -11,20 +11,20 @@ func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Println("Logs from your program will appear here!")
 
-    // Creates TCP server listening on port 4221	
+	 // Uncomment this block to pass the first stage
+	
 	 l, err := net.Listen("tcp", "0.0.0.0:4221")
 	 if err != nil {
 	 	fmt.Println("Failed to bind to port 4221")
 	 	os.Exit(1)
 	 }
 	
-     portal, err := l.Accept()
+     conn, err := l.Accept()
 	 if err != nil {
 	 	fmt.Println("Error accepting connection: ", err.Error())
 	 	os.Exit(1)
 	 }
-
-     handleConnection(portal)
+     defer conn.Close()
 }
 
 func handleConnection(conn net.Conn) {
@@ -51,7 +51,7 @@ func handleConnection(conn net.Conn) {
 
     _, err = conn.Write([]byte(response))
     if err != nil {
-        fmt.Println("Failed to write response")
+        fmt.Println("Error writing response: ", err.Error())
         os.Exit(1)
     }
 
