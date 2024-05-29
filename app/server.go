59e60577d830b9fd6164682c6b7aede7e2608621
Andrew Hennis <dawnandrew100@gmail.com>
2024-05-29 17:47:35 -0400

    compressed Response redo

--- app/server.go
+++ app/server.go
@@ -122,5 +122,5 @@ func responseBuilder(statusLine string, contentType string, contentLength int, b
 }
 
 func compressedResponseBuilder(contentEncoding string, statusLine string, contentType string, contentLength int, body string) string {
-    return fmt.Sprintf("%sContent-Encoding: %s\r\nContent-Type: %s\r\nContent-Length: %d\r\n\r\n%s", statusLine, contentEncoding, contentType, contentLength, body)
+    return fmt.Sprintf("%sContent-Encoding: %s\nContent-Type: %s\r\nContent-Length: %d\r\n\r\n%s", statusLine, contentEncoding, contentType, contentLength, body)
 }
