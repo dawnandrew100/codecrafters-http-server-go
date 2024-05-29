af5749255656c342251e06184ec7a66abb090563
Andrew Hennis <dawnandrew100@gmail.com>
2024-05-28 20:07:19 -0400

    debugging

--- app/server.go
+++ app/server.go
@@ -49,7 +49,9 @@ func handleConnection(conn net.Conn) {
     if path == "/" {
         response = "HTTP/1.1 200 OK\r\n\r\n"
     }
+    fmt.Println("Good till contains")
     if strings.Contains(path, "echo") {
+        fmt.Println(path)
         echostring := strings.Split(path, "/")
         response = "HTTP/1.1 200 OK\r\n"
         response += fmt.Sprintf("Content-Type: text/plain\r\nContent-Length: %d\r\n\r\n", len(echostring[1]))
