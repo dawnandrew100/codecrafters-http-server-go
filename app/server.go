605ba839fb14f04c6763ace136e4ba9e46d93e13
Andrew Hennis <dawnandrew100@gmail.com>
2024-05-29 00:41:30 -0400

    redo

--- app/server.go
+++ app/server.go
@@ -10,7 +10,7 @@ import (
 
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	fmt.Println("Logs from your program will appear here!")
+	fmt.Println("Logs from your program will appear here!!")
 
 	 // Uncomment this block to pass the first stage
 	 l, err := net.Listen("tcp", "0.0.0.0:4221")
