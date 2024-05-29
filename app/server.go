5cb786a589a284f65bfa23afdddbd656d1098bc6
Andrew Hennis <dawnandrew100@gmail.com>
2024-05-28 23:43:05 -0400

    Hopefully fixed thing I broke

--- app/server.go
+++ app/server.go
@@ -66,7 +66,7 @@ func handleConnection(conn net.Conn) {
         response += fmt.Sprintf("Content-Type: text/plain\r\nContent-Length: %d\r\n\r\n", len(user_agent_echo[1])-1)
         response +=user_agent_echo[1]
 
-    case method == GET && strings.Contains(path, "files"):
+    case method == "GET" && strings.Contains(path, "files"):
         directory := os.Args[2]
         fileName := strings.TrimPrefix(path, "/files/")
         data, err := os.ReadFile(directory + fileName)
