d31770d120d81136330544812f4532442e0c0342
Andrew Hennis <dawnandrew100@gmail.com>
2024-05-29 00:44:06 -0400

    buf equals

--- app/server.go
+++ app/server.go
@@ -10,7 +10,7 @@ import (
 
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	fmt.Println("Logs from your program will appear here!!")
+	fmt.Println("Logs from your program will appear here!")
 
 	 // Uncomment this block to pass the first stage
 	 l, err := net.Listen("tcp", "0.0.0.0:4221")
@@ -37,7 +37,7 @@ func handleConnection(conn net.Conn) {
     
     buf := make([]byte, 1024)
     conn.Read(buf)
-    bytes.Trim(buf, "\x00")
+    buf = bytes.Trim(buf, "\x00")
 
     bufString := strings.Split(string(buf), "\n")
     request := strings.Split(bufString[0], " ")
